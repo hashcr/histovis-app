3956f37920e6d2a9fc25751a3328b559c8993f4f
Ashuin Sharma <ashuin.sharma@gmail.com>
2025-10-19 01:46:48 -0600

    [menu] Managing menu options permissions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,6 @@ export class AppComponent {
     { title: 'Upload', url: '/folder/upload', icon: 'cloud-upload' },
     { title: 'Search', url: '/folder/search', icon: 'search' },
     { title: 'Analyze', url: '/folder/analyze', icon: 'build' },
-    { title: 'Admin', url: '/folder/admin', icon: 'settings' },
-    { title: 'Logout', url: '/logout', icon: 'log-out' },
   ];
 
   constructor() {
