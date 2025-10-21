3a5b8fe554c91210eadc96d927f6be187b020883
Ashuin Sharma <ashuin.sharma@gmail.com>
2025-10-20 18:49:48 -0600

    [search] Fix image-card tests

--- src/app/features/search/image-card/image-card.component.spec.ts
+++ src/app/features/search/image-card/image-card.component.spec.ts
@@ -1,22 +1,45 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImageCardComponent } from './image-card.component';
+import { Component, Input } from '@angular/core';
+import { ImageInfo } from 'src/app/core/models/image.model';
 
 describe('ImageCardComponent', () => {
-  let component: ImageCardComponent;
-  let fixture: ComponentFixture<ImageCardComponent>;
+  let fixture: ComponentFixture<any>;
+  let hostComponent: TestHostComponent;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [ImageCardComponent],
+      imports: [TestHostComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ImageCardComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = fixture.componentInstance;
+
+    const dummyImage: ImageInfo = {
+      id: '1',
+      fileName: 'example.jpg',
+      url: 'http://example.com/image.jpg',
+      title: 'Example Image',
+      description: 'This is an example image.',
+      tagsList: ['example', 'image'],
+      imageFile: null,
+    };
+    hostComponent.image = dummyImage;
+
     fixture.detectChanges();
   }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(hostComponent).toBeTruthy();
   });
 });
+
+@Component({
+  standalone: true,
+  template: ` <app-image-card [image]="image"></app-image-card>`,
+  imports: [ImageCardComponent],
+})
+class TestHostComponent {
+  @Input() image!: ImageInfo;
+}
