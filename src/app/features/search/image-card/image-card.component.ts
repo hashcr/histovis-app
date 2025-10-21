b5213dd111f5a1efdbcb0fce2f571608fc79808c
Ashuin Sharma <ashuin.sharma@gmail.com>
2025-10-21 02:25:02 -0600

    [analyze] Add clickable cards

--- src/app/features/search/image-card/image-card.component.ts
+++ src/app/features/search/image-card/image-card.component.ts
@@ -2,13 +2,14 @@ import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonCard, IonImg, IonCardHeader, IonText, IonChip, IonLabel, IonCardContent, IonCardTitle } from '@ionic/angular/standalone';
 import { ImageInfo } from 'src/app/core/models/image.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-image-card',
   templateUrl: './image-card.component.html',
   styleUrls: ['./image-card.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonCard, IonImg, IonCardHeader, IonText, IonChip, IonLabel, IonCardContent, IonCardTitle],
+  imports: [CommonModule, IonCard, IonImg, IonCardHeader, IonText, IonChip, IonLabel, IonCardContent, IonCardTitle, RouterModule],
 })
 export class ImageCardComponent  {
   // Input Signals
