35a806e8cbbbbb73e4a27de41e6e397bb5b6442c
Ashuin Sharma <ashuin.sharma@gmail.com>
2025-10-27 23:43:08 -0600

    [search] Tags selector tests

--- src/app/features/shared/components/tags-selector/tags-selector.component.spec.ts
+++ src/app/features/shared/components/tags-selector/tags-selector.component.spec.ts
@@ -1,18 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TagsSelectorComponent } from './tags-selector.component';
+import { ImageFormFormService } from '../../../upload/image-form/image-form.form.service';
+import { FormArray, FormControl } from '@angular/forms';
+import { Component, Input } from '@angular/core';
 
 describe('TagsSelectorComponent', () => {
-  let component: TagsSelectorComponent;
-  let fixture: ComponentFixture<TagsSelectorComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let imageFormServiceMock: jasmine.SpyObj<ImageFormFormService>;
 
   beforeEach(waitForAsync(() => {
+    imageFormServiceMock = jasmine.createSpyObj('ImageFormFormService', ['getTags', 'addTag', 'removeTag']);
     TestBed.configureTestingModule({
-      imports: [TagsSelectorComponent],
+      imports: [TestHostComponent],
+      providers: [
+        { provide: ImageFormFormService, useValue: imageFormServiceMock }
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TagsSelectorComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+    component.tags = new FormArray<FormControl<string>>([]);
+
     fixture.detectChanges();
   }));
 
@@ -20,3 +30,12 @@ describe('TagsSelectorComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  standalone: true,
+  template: ` <app-tags-selector [tagsArray]="tags"></app-tags-selector>`,
+  imports: [TagsSelectorComponent],
+})
+class TestHostComponent {
+  @Input() tags!: FormArray<FormControl<string>>;
+}
