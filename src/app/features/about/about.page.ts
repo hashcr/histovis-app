4a6f0b4a47f78c5ed6a73d225d253255aaf25464
Ashuin Sharma <ashuin.sharma@gmail.com>
2025-10-28 19:32:50 -0600

    [about] Add original project objectives

--- src/app/features/about/about.page.ts
+++ src/app/features/about/about.page.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonItem,IonText, IonIcon, IonFooter, IonList, IonLabel, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { IonToggle, IonBadge, IonItem,IonText, IonIcon, IonFooter, IonList, IonLabel, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
   standalone: true,
-  imports: [IonItem, IonText, IonFooter, IonList, IonIcon, IonCardHeader, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, FormsModule]
+  imports: [IonToggle, IonBadge, IonItem, IonText, IonFooter, IonList, IonIcon, IonCardHeader, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, FormsModule]
 })
 export class AboutPage implements OnInit {
+
+  // Signals
+  objectivesToggledOn = signal(false);
 
   constructor() { }
 
