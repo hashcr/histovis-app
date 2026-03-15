6efd6fe3c6ef723993286169ff2a692af41a15fa
Ashuin Sharma Chamorro <81604231+hashcr@users.noreply.github.com>
2026-03-15 11:00:57 -0600

    Adding missing icon refs for About page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonAvatar, IonChip } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { cloudUploadSharp, cloudUploadOutline, buildSharp, buildOutline, settingsSharp, settingsOutline, logOutSharp, logOutOutline, searchSharp, searchOutline, homeSharp, homeOutline, closeCircleOutline, informationCircleOutline, informationCircleSharp, sendSharp, personCircleOutline, trashSharp } from 'ionicons/icons';
+import { cloudUploadSharp, cloudUploadOutline, buildSharp, buildOutline, settingsSharp, settingsOutline, logOutSharp, logOutOutline, searchSharp, searchOutline, homeSharp, homeOutline, closeCircleOutline, informationCircleOutline, informationCircleSharp, sendSharp, personCircleOutline, trashSharp, mailOutline, logoGithub, globeOutline } from 'ionicons/icons';
 import { User } from './core/models/user.model';
 import { AuthService } from './core/auth/auth.service';
 
@@ -42,7 +42,10 @@ export class AppComponent {
       'information-circle-sharp': informationCircleSharp,
       'send-sharp': sendSharp,
       'person-circle-outline': personCircleOutline,
-      'trash-sharp': trashSharp
+      'trash-sharp': trashSharp,
+      'mail-outline': mailOutline,
+      'logo-github': logoGithub,
+      'globe-outline': globeOutline
     });
   }
 
