fb841c260e305216fc52f5a64f77cd5be71947a4
Ashuin Sharma Chamorro <81604231+hashcr@users.noreply.github.com>
2026-05-31 16:40:38 -0600

    [about] Correct corpus and other links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonAvatar, IonChip } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { cloudUploadSharp, cloudUploadOutline, buildSharp, buildOutline, settingsSharp, settingsOutline, logOutSharp, logOutOutline, searchSharp, searchOutline, homeSharp, homeOutline, closeCircleOutline, informationCircleOutline, informationCircleSharp, sendSharp, personCircleOutline, trashSharp, mailOutline, logoGithub, globeOutline, alertCircleOutline, checkmarkCircleOutline, close, sparkles, chevronBack, chevronDownOutline, refreshOutline } from 'ionicons/icons';
+import { cloudUploadSharp, cloudUploadOutline, buildSharp, buildOutline, settingsSharp, settingsOutline, logOutSharp, logOutOutline, searchSharp, searchOutline, homeSharp, homeOutline, closeCircleOutline, informationCircleOutline, informationCircleSharp, sendSharp, personCircleOutline, trashSharp, mailOutline, logoGithub, logoLinkedin, globeOutline, alertCircleOutline, checkmarkCircleOutline, close, sparkles, chevronBack, chevronDownOutline, refreshOutline } from 'ionicons/icons';
+
 import { User } from './core/models/user.model';
 import { AuthService } from './core/auth/auth.service';
 
@@ -45,6 +46,7 @@ export class AppComponent {
       'trash-sharp': trashSharp,
       'mail-outline': mailOutline,
       'logo-github': logoGithub,
+      'logo-linkedin': logoLinkedin,
       'globe-outline': globeOutline,
       'alert-circle-outline': alertCircleOutline,
       'checkmark-circle-outline': checkmarkCircleOutline,
